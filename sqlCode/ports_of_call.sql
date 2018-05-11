b801d436b737aff436c0ea7c82be542d9e9a004d
Christian Lowe <christglowe@gmail.com>
2018-05-10 22:44:24 -0500

    Add more port of call information

--- sqlCode/ports_of_call.sql
+++ sqlCode/ports_of_call.sql
@@ -12,28 +12,59 @@
 
 -- INSERT INTO port VALUES (DEFAULT, 'Honolulu', 'USA', null, 21.3069, 157.8583, 'spring', 2016, 'Hawaii');
 
-INSERT INTO port VALUES (DEFAULT, 'Yokohama', 'Japan', null, 35.4437, 139.6380, 'spring', 2016, 'Kanagawa Prefecture');
+-- INSERT INTO port VALUES (DEFAULT, 'Yokohama', 'Japan', null, 35.4437, 139.6380, 'spring', 2016, 'Kanagawa Prefecture');
+--
+-- INSERT INTO port VALUES (DEFAULT, 'Kobe', 'Japan', null, 34.6901, 135.1955, 'spring', 2016, 'Hyogo Prefecture');
+--
+-- INSERT INTO port VALUES (DEFAULT, 'Shanghai', 'China', null, 31.2304, 121.4737, 'spring', 2016, null);
+--
+-- INSERT INTO port VALUES (DEFAULT, 'Hong Kong', 'China', null, 22.3964, 114.1095, 'spring', 2016, null);
+--
+-- INSERT INTO port VALUES (DEFAULT, 'Ho Chi Minh City', 'Vietnam', null, 10.8231, 106.6297, 'spring', 2016, null);
+--
+-- INSERT INTO port VALUES (DEFAULT, 'Yangon', 'Myanmar', null, 16.8661, 96.1951, 'spring', 2016, null);
+--
+-- INSERT INTO port VALUES (DEFAULT, 'Kochi', 'India', null, 9.9312, 76.2673, 'spring', 2016, 'Kerala');
+--
+-- INSERT INTO port VALUES (DEFAULT, 'Port Louis', 'Mauritius', null, -20.1609, 57.5012, 'spring', 2016, null);
+--
+-- INSERT INTO port VALUES (DEFAULT, 'Cape Town', 'South Africa', null, -33.9249, 18.4241, 'spring', 2016, 'Western Cape');
+--
+-- INSERT INTO port VALUES (DEFAULT, 'Takoradi', 'Ghana', null, 4.9016, -1.7831, 'spring', 2016, null);
+--
+-- INSERT INTO port VALUES (DEFAULT, 'Accra', 'Ghana', null, 5.6037, -0.1870, 'spring', 2016, null);
+--
+-- INSERT INTO port VALUES (DEFAULT, 'Casablanca', 'Morocco', null, 33.5731, -7.5898, 'spring', 2016, 'Casablanca-Settat');
+--
+-- INSERT INTO port VALUES (DEFAULT, 'Southampton', 'England', null, 50.9097, -1.4044, 'spring', 2016, 'South East England');
 
-INSERT INTO port VALUES (DEFAULT, 'Kobe', 'Japan', null, 34.6901, 135.1955, 'spring', 2016, 'Hyogo Prefecture');
+-- INSERT INTO port VALUES (default, 'Civitavecchia', 'Italy', null, 42.0924, 11.7954, 'fall', 2015, 'Lazio');
+-- INSERT INTO port VALUES (DEFAULT, 'Naples', 'Italy', NULL, 40.8518, 14.2681, 'fall', 2015, 'Campania');
+-- INSERT INTO port VALUES (DEFAULT, 'Dubrovnik', 'Croatia', NULL, 42.6507, 18.0944, 'fall', 2015, 'Dubrovnik-Neretva');
+-- INSERT INTO port VALUES (DEFAULT, 'Piraeus', 'Greece', NULL, 37.9430, 23.6470, 'fall', 2015, 'Attica');
+--
+-- INSERT INTO port VALUES (DEFAULT, 'Valencia', 'Spain', NULL, 39.4699, 0.3763, 'fall', 2015, 'Valencia');
+--
+-- INSERT INTO port VALUES (DEFAULT, 'Casablanca', 'Morocco', null, 33.5731, -7.5898, 'fall', 2015, 'Casablanca-Settat');
+--
+-- INSERT INTO port VALUES (DEFAULT, 'Dakar', 'Senegal', null, 14.7167, -17.4677, 'fall', 2015, 'Dakar');
+--
+-- INSERT INTO port VALUES (DEFAULT, 'Salvador', 'Brazil', null, -12.9722, -38.5014, 'fall', 2015, 'Bahia');
+--
+-- INSERT INTO port VALUES (DEFAULT, 'Port of Spain', 'Trinidad and Tobago', null, 10.6549, -61.5019, 'fall', 2015, NULL);
+--
+-- INSERT INTO port VALUES (DEFAULT, 'Panama Canal', 'Panama', null, 8.9824, -79.5199, 'fall', 2015, NULL);
+--
+-- INSERT INTO port VALUES (DEFAULT, 'Puntarenas', 'Costa Rica', null, 9.9778, -84.8294, 'fall', 2015, 'Puntarenas Province');
 
-INSERT INTO port VALUES (DEFAULT, 'Shanghai', 'China', null, 31.2304, 121.4737, 'spring', 2016, null);
-
-INSERT INTO port VALUES (DEFAULT, 'Hong Kong', 'China', null, 22.3964, 114.1095, 'spring', 2016, null);
-
-INSERT INTO port VALUES (DEFAULT, 'Ho Chi Minh City', 'Vietnam', null, 10.8231, 106.6297, 'spring', 2016, null);
-
-INSERT INTO port VALUES (DEFAULT, 'Yangon', 'Myanmar', null, 16.8661, 96.1951, 'spring', 2016, null);
-
-INSERT INTO port VALUES (DEFAULT, 'Kochi', 'India', null, 9.9312, 76.2673, 'spring', 2016, 'Kerala');
-
-INSERT INTO port VALUES (DEFAULT, 'Port Louis', 'Mauritius', null, -20.1609, 57.5012, 'spring', 2016, null);
-
-INSERT INTO port VALUES (DEFAULT, 'Cape Town', 'South Africa', null, -33.9249, 18.4241, 'spring', 2016, 'Western Cape');
-
-INSERT INTO port VALUES (DEFAULT, 'Takoradi', 'Ghana', null, 4.9016, -1.7831, 'spring', 2016, null);
-
-INSERT INTO port VALUES (DEFAULT, 'Accra', 'Ghana', null, 5.6037, -0.1870, 'spring', 2016, null);
-
-INSERT INTO port VALUES (DEFAULT, 'Casablanca', 'Morocco', null, 33.5731, -7.5898, 'spring', 2016, 'Casablanca-Settat');
-
-INSERT INTO port VALUES (DEFAULT, 'Southampton', 'England', null, 50.9097, -1.4044, 'spring', 2016, 'South East England');
+INSERT INTO port VALUES (DEFAULT, 'Barcelona', 'Spain', null, 41.3851, 2.1734, 'fall', 2017, 'Barcelona');
+INSERT INTO port VALUES (DEFAULT, 'Valencia', 'Spain', NULL, 39.4699, 0.3763, 'fall', 2017, 'Valencia');
+INSERT INTO port VALUES (DEFAULT, 'Tema', 'Ghana', NULL, 5.7348, 0.0302, 'fall', 2017, 'Tema');
+INSERT INTO port VALUES (DEFAULT, 'Takoradi', 'Ghana', null, 4.9016, -1.7831, 'fall', 2017, null);
+INSERT INTO port VALUES (DEFAULT, 'Cape Town', 'South Africa', null, -33.9249, 18.4241, 'fall', 2017, 'Western Cape');
+ INSERT INTO port VALUES (DEFAULT, 'Kochi', 'India', null, 9.9312, 76.2673, 'fall', 2017, 'Kerala');
+ INSERT INTO port VALUES (DEFAULT, 'Yangon', 'Myanmar', null, 16.8661, 96.1951, 'fall', 2017, null);
+ INSERT INTO port VALUES (DEFAULT, 'Ho Chi Minh City', 'Vietnam', null, 10.8231, 106.6297, 'fall', 2017, null);
+ INSERT INTO port VALUES (DEFAULT, 'Shanghai', 'China', null, 31.2304, 121.4737, 'fall', 2017, null);
+ INSERT INTO port VALUES (DEFAULT, 'Kobe', 'Japan', null, 34.6901, 135.1955, 'fall', 2017, 'Hyogo Prefecture');
+ INSERT INTO port VALUES (DEFAULT, 'Honolulu', 'USA', null, 21.3069, 157.8583, 'fall', 2017, 'Hawaii');
